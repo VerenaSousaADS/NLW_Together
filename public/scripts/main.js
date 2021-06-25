d4c75a35d61baada5ae1d400ba90b89e1ce1f90e
Verena Sousa <verena.ads19@hotmail.com>
2021-06-25 11:51:31 -0300

    Modificações em main.js, route.js e room.ejs

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,6 +5,11 @@ import  Modal  from './modal.js'
 // Adicionar a Modal em uma constante
 const modal = Modal()
 
+// Mapeamento 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 // Pegar todos os botões que existe com a classe check
 
 const checkButtons = document.querySelectorAll(".actions a.check")
@@ -14,21 +19,35 @@ checkButtons.forEach(button => {
    
     // adicionar a escuta 
     // o que será escutado, será o evento click
-    button.addEventListener("click", event => {
-
-        // Abrir a modal
-        modal.open()
-    })
+    button.addEventListener("click", handleClick)
 
 })
 
 // Quando o botão delete for clicado ele abre a modal
 
-const deleteButton = document.querySelectorAll(".actions .delete")
+const deleteButton = document.querySelectorAll(".actions a.delete")
 
 deleteButton.forEach(button => {
-    button.addEventListener("click", event =>{
-        modal.open()
-    })
+    button.addEventListener("click", (event) => handleClick(event, false))
 })
 
+function handleClick(event, check = true){
+    event.preventDefault()
+    const text = check ? "Marcar como lida" : "Excluir"
+    const slug = check ? "check" : "delete"
+
+    const roomId = document.querySelector("#room-id").dataset.id
+
+    const questionId = event.target.dataset.id
+
+    const form = document.querySelector(".modal form")
+    form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`)
+
+    // Concatenação da constante de text com o "esta pergunta"
+    modalTitle.innerHTML = `${text} esta pergunta` 
+    modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text.toLocaleLowerCase()}`
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+     // abrir modal
+    modal.open()
+}
--- src/route.js
+++ src/route.js
@@ -8,11 +8,23 @@ const route = express.Router()
 // o req é a requisição da rota e o res o response, ou resposta, que a função vai devolver
 
 //  a barra / , é a mesma localizada depois de uma url, dominio, e indica a página de index de determinado site, nesse caso desta aplicação
+// get = pegar dados
 route.get('/', (req, res) => res.render("index"))
 
 route.get('/room', (req,res) => res.render("room"))
 
 route.get('/create-pass', (req, res) => res.render("create-pass"))
+
+route.get('/room/:room', (req, res) => res.render("room"))
+
+
+// Formato que o formulário de dentro da modal tem que passar a informação
+// Passa para o express uma variavel da qual não sabemos o valor, já o primeiro room é fixo
+// post =  enviar dados
+// route.post('/room/:room/:question/:action', (req, res) => req.render())
+
+
+
 
 // Exportar a constante route
 module.exports = route
